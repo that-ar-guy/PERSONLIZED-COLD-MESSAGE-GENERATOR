58114bd7df73ff9e18a530fa5b26e8fc01065bb5
that-ar-guy <mohdarahman124@gmail.com>
2024-12-09 21:43:28 +0530

    basic is done

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template("index.html")
+
+@app.route("/process", methods=["POST"])
+def process_form():
+    company_url = request.form.get("company_url")
+    job_position = request.form.get("job_position")
+    recipient = request.form.get("recipient")
+    resume_attached = "resume_attached" in request.form
+
+    # For now, just return the submitted data for verification
+    return f"""
+    Company URL: {company_url}<br>
+    Job Position: {job_position}<br>
+    Recipient: {recipient}<br>
+    Resume Attached: {'Yes' if resume_attached else 'No'}
+    """
 
 if __name__ == '__main__':
     app.run(debug=True)
